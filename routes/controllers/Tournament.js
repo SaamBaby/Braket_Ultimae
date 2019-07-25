a4ebd46e731dbdfd08841a730cf397c8ecc490d7
Sam Baby <sambaby870@gmail.com>
2019-07-25 19:06:54 -0400

    Implemented back end Create

--- routes/controllers/Tournament.js
+++ routes/controllers/Tournament.js
@@ -6,11 +6,11 @@ const Tournament = require('../../models/Tournament');
 exports.createNewTournament = async (req, res) => {
   const body = req.body;
   const tournament = await new Tournament (body).save();
-  console.log(res); 
+  
    console.log(res.body)
  // res.redirect(`/projects/${tournament._id}`); testing
   // res.redirect(`http://localhost:3000/tournament/`);
-  console.log("saved");
+  
 };
 
 
