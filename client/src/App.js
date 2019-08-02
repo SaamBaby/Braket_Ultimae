86cd7ae5f1f14deb5c6bbb2a8c94ca7ab2ac3acb
Thomas <Mathew>
2019-08-02 15:59:28 -0400

    finished authentication frontend
    sitjo

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,11 @@ import Tournament from '../src/view/Tournament';
 import {Route, Switch}  from 'react-router-dom';
 import Create from './components/create';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Details from './components/bracket_details';
+import Login from './components/Login';
+import Register from './components/Register'
+import { rootCertificates } from 'tls';
 
 
 
@@ -22,13 +26,14 @@ function App() {
  <Navbar></Navbar>
 
    <Switch>
-   <Route exact path="/" component={Home}/>
+   <Route exact  path="/" component={Home}/>
    <Route exact  path="/tournament" component={Tournament}/>
    <Route exact  path="/tournament/:id" component={Details}/>
    <Route exact  path="/create-tournament" component={Create}/>
-   
+   <Route exact  path="/login" component={Login}/>
+   <Route exact  path="/register" component={Register}/>
    </Switch>
-   
+<Footer></Footer>
    </>
 
   );
