e79b0d507e684582db49beca96f4d85d820898e4
Sam Baby <sambaby870@gmail.com>
2019-08-07 19:14:45 -0400

    bracket tournament hero deployment

---
+++ client/build/precache-manifest.47459ee1215221ca878d126db1bfd8e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21cadc06ce9df91d4c7aa617720b3b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5e33c51f943f1be992a",
+    "url": "/static/css/main.8fa47617.chunk.css"
+  },
+  {
+    "revision": "e15981c5201771012749",
+    "url": "/static/js/2.3bb37b3c.chunk.js"
+  },
+  {
+    "revision": "a5e33c51f943f1be992a",
+    "url": "/static/js/main.4906edcb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/static/media/logo.9f0cccc1.svg"
+  }
+]);
